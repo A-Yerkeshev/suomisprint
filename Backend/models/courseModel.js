544672ccbc2bc6a0bd160833e623c88c3517c4cf
Arman Yerkesh <a.yerkesh@gmail.com>
2023-09-15 08:37:56 +0300

    Add CourseModel

---
+++ Backend/models/courseModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const courseSchema = new Schema({
+  title: {type: String, required: true},
+  provider_id: {type: Schema.ObjectId}, // Will be required later
+  description: {type: String},
+  price: {type: Number, required: true},
+  max_students: {type: Number},
+  enrolled: {type: Number},
+  start_date: {type: Date, required: true},
+  end_date: {type: Date, required: true},
+  start_time: {type: Number, required: true},
+  end_time: {type: Number, required: true}
+})
+
+module.exports = mongoose.model('Course', courseSchema)
