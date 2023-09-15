2a2c75a00ed7ad91aaa2cb1db9cb22b76f84d771
Arman Yerkesh <a.yerkesh@gmail.com>
2023-09-15 12:06:04 +0300

    Validate values in PATCH route

--- Backend/controllers/coursesController.js
+++ Backend/controllers/coursesController.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 // GET /api/courses
 const list = async (req, res) => {
-  const courses = await Course.find({}).sort({createdAt: -1});
+  const courses = await Course.find({}).sort({updatedAt: -1});
   res.status(200).json(courses);
 }
 
@@ -107,7 +107,7 @@ const create = async (req, res) => {
     const course = await Course.create(payload);
     res.status(200).json(course);
   } catch (err) {
-    res.status(500).json({error: `Failed to save new course. Error: `});
+    res.status(500).json({error: `Failed to save new course. Error: ${err}`});
   }
 }
 
@@ -115,19 +115,74 @@ const create = async (req, res) => {
 const update = async (req, res) => {
   const {id} = req.params;
 
+  let {title,
+    providerId,
+    description,
+    shortDescription,
+    level,
+    price,
+    imageUrl,
+    maxStudents,
+    startDate,
+    endDate,
+    startTime,
+    endTime} = req.body;
+
+  // Validate data types
+  try {
+    if (price) { price = parseFloat(price); }
+  } catch(err) {
+    res.status(400).json({error: `Failed to convert price/maxStudents/startTime/endTime into a number. Error: ${err}`});
+    return;
+  }
+
+  try {
+    if (startDate) { startDate = new Date(JSON.parse(startDate)); }
+    if (endDate) { endDate = new Date(JSON.parse(endDate)); }
+  } catch(err) {
+    res.status(400).json({error: `Failed to convert startDate/endDate to date. Error: ${err}`});
+    return;
+  }
+
+  if (providerId && !validId(providerId)) {
+    res.status(400).json({error: `${providerId} is not valid value for providerId.`});
+    return;
+  }
+
   if (!validId(id)) {
     res.status(400).json({error: `${id} is not a valid course id.`});
     return;
   }
 
-  const course = await Course.findOneAndUpdate({_id: id}, {...req.body}, {new: true});
+  // Update course
+  try {
+    const payload = {
+      title,
+      provider_id: providerId,
+      description,
+      short_description: shortDescription,
+      image_url: imageUrl,
+      level,
+      price,
+      max_students: maxStudents,
+      start_date: startDate,
+      end_date: endDate,
+      start_time: startTime,
+      end_time: endTime
+    }
+    Object.keys(payload).forEach(key => payload[key] === undefined ? delete payload[key] : {});
 
-  if(!course) {
-    res.status(404).json({error: `Course with id ${id} was not found.`});
-    return;
+    const course = await Course.findOneAndUpdate({_id: id}, payload, {new: true});
+
+    if (!course) {
+      res.status(404).json({error: `Course with id ${id} was not found.`});
+      return;
+    }
+
+    res.status(200).json(course);
+  } catch (err) {
+    res.status(500).json({error: `Failed to save updates to the course. Error: ${err}`});
   }
-
-  res.status(200).json(course);
 }
 
 const validId = (id) => {
