da172e6ba25b33834fae7652216d9b25a163c1eb
Mamita <mamitagurung895@gmail.com>
2023-09-15 10:31:33 +0300

    Add About.js and About.css

--- Frontend/src/pages/About.js
+++ Frontend/src/pages/About.js
@@ -1,11 +1,48 @@
 // src/components/About.js
+import React from 'react';
 
 function About() {
     return (
-      <div>
-        <h2>About Page</h2>
-        <p>Learn more about our company.</p>
-      </div>
+      <section>
+        <div>
+          <h1>About Us</h1>
+        </div>
+
+        <div> 
+          <h2>Suomisprint-Your gateway to Finnish language mastery.</h2>
+          
+          <p>
+          Welcome to SuomiSprint, your gateway to mastering the beautiful Finnish language 
+          with ease and confidence. At SuomiSprint, we are passionate about helping learners 
+          from around the world embark on their Finnish language journey and reach their linguistic 
+          goals.
+          </p>
+        </div>
+        <div>
+          <h3> Why choose SuomiSprint? </h3>
+          <ul>
+          
+            <li>
+            Expert Instructors: Our team of experienced Finnish language instructors are dedicated 
+            to providing engaging and personalized lessons.
+            </li>
+            <li> 
+            Cutting-Edge Technology: We harness the power of technology to create interactive and innovative learning experiences.
+            </li>
+            
+            <li>
+            Flexible Learning: Whether you're a beginner or an advanced learner, our flexible course options cater to your specific needs and schedule.
+            </li>
+            <li>
+            Supportive Community: Join a vibrant community of fellow learners and enthusiasts, where you can practice your skills and exchange cultural insights
+            </li>
+            <li>
+            Proven Results: Our students achieve remarkable progress in their language skills, enabling them to communicate confidently in Finnish.
+            </li>
+          </ul>
+        </div>
+
+        </section>
     );
   }
   
