264fa6a4f77c26eb9c78537eb120aaec3de61bd7
Arman Yerkesh <a.yerkesh@gmail.com>
2023-09-15 10:04:59 +0300

    Add route for DELETE /api/courses/:id

--- Backend/controllers/coursesController.js
+++ Backend/controllers/coursesController.js
@@ -1,30 +1,51 @@
 const Course = require('../models/courseModel');
 const mongoose = require('mongoose');
 
-// GET /courses
+// GET /api/courses
 const list = async (req, res) => {
   const courses = await Course.find({}).sort({createdAt: -1});
   res.status(200).json(courses);
 }
 
-// GET /courses/:id
+// GET /api/courses/:id
 const get = async (req, res) => {
   const {id}  = req.params;
 
   if (!validId(id)) {
-    return res.status(400).json({error: `${id} is not a valid course id.`});
+    res.status(400).json({error: `${id} is not a valid course id.`});
+    return;
   }
 
   const course = await Course.findById(id);
 
   if (!course) {
-    return res.status(404).json({error: `Course with id ${id} was not found.`});
+    res.status(404).json({error: `Course with id ${id} was not found.`});
+    return;
   }
 
   res.status(200).json(course);
 }
 
-// POST /courses
+// DELETE /api/courses/:id
+const remove = async (req, res) => {
+  const {id} = req.params;
+
+  if (!validId(id)) {
+    res.status(400).json({error: `${id} is not a valid course id.`});
+    return;
+  }
+
+  const course = await Course.findOneAndDelete({_id: id})
+
+  if(!course) {
+    res.status(404).json({error: `Course with id ${id} was not found.`});
+    return;
+  }
+
+  res.status(200).json(course);
+}
+
+// POST /api/courses
 const create = async (req, res) => {
   let {title,
     providerId,
@@ -86,11 +107,6 @@ const create = async (req, res) => {
   } catch (err) {
     res.status(500).json({error: `Failed to save new course. Error: `});
   }
-}
-
-// delete a blog
-const remove = async (req, res) => {
-  // Your code here
 }
 
 // Update blog using PATCH
