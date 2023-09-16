fd3060ad847158e715d69dbde48e1dfce4209664
Arman Yerkesh <a.yerkesh@gmail.com>
2023-09-16 09:07:17 +0300

    Change courseModel's schema

--- Backend/models/courseModel.js
+++ Backend/models/courseModel.js
@@ -5,17 +5,17 @@ const Schema = mongoose.Schema
 const courseSchema = new Schema({
   title: {type: String, required: true},
   provider_id: {type: Schema.ObjectId}, // Will be required later
-  description: {type: String},
-  short_description: {type: String},
-  level: {type: String},
+  description: {type: String, required: true},
+  short_description: {type: String, required: true},
+  level: {type: String, required: true},
   image_url: {type: String},
-  price: {type: Number, required: true},
+  price: {type: Number},
   max_students: {type: Number},
-  enrolled: {type: Number},
-  start_date: {type: Date, required: true},
-  end_date: {type: Date, required: true},
-  start_time: {type: Number, required: true},
-  end_time: {type: Number, required: true}
+  enrolled: {type: Array},
+  start_date: {type: Date},
+  end_date: {type: Date},
+  start_time: {type: Number},
+  end_time: {type: Number}
 }, {timestamps: true})
 
 module.exports = mongoose.model('Course', courseSchema)
