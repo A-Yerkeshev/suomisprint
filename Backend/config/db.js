b2142c4f28c96f5249acd66c58885634b27880f0
Trang Vu <Trang.Vu3@metropolia.fi>
2023-09-19 16:48:27 +0300

    Add a page for teachers

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,10 +1,22 @@
-const mongoose = require('mongoose')
-const MONGO_URI = process.env.MONGO_URI
+const mongoose = require("mongoose");
+const MONGO_URI = process.env.MONGO_URI;
+
+// full connection string
 
 const connectDB = async () => {
-    const conn = await mongoose.connect(MONGO_URI)
-    console.log(`Connected to database`)
-}
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+    });
+    console.log(`Connected to database`);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+// const connectDB = async () => {
+//     const conn = await mongoose.connect(MONGO_URI)
+//     console.log(`Connected to database`)
+// }
 
-module.exports = connectDB
+module.exports = connectDB;
--- Backend/models/courseModel.js
+++ Backend/models/courseModel.js
@@ -1,21 +1,24 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-const courseSchema = new Schema({
-  title: {type: String, required: true},
-  provider_id: {type: Schema.ObjectId}, // Will be required later
-  description: {type: String, required: true},
-  short_description: {type: String, required: true},
-  level: {type: String, required: true},
-  image_url: {type: String},
-  price: {type: Number},
-  max_students: {type: Number},
-  enrolled: {type: Array},
-  start_date: {type: Date},
-  end_date: {type: Date},
-  start_time: {type: Number},
-  end_time: {type: Number}
-}, {timestamps: true})
+const courseSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    provider_id: { type: Schema.ObjectId }, // Will be required later
+    description: { type: String },
+    short_description: { type: String },
+    level: { type: String },
+    image_url: { type: String },
+    price: { type: Number },
+    max_students: { type: Number },
+    enrolled: { type: Array },
+    start_date: { type: Date },
+    end_date: { type: Date },
+    start_time: { type: Number },
+    end_time: { type: Number },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Course', courseSchema)
+module.exports = mongoose.model("Course", courseSchema);
