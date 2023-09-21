986f605ae1e16238fa7e269f694ef8acfcc3aec7
Arman Yerkesh <a.yerkesh@gmail.com>
2023-09-21 13:45:46 +0300

    Add "register" action to usersController

--- Backend/controllers/usersController.js
+++ Backend/controllers/usersController.js
@@ -1,121 +1,37 @@
-const Course = require('../models/courseModel');
+const User = require('../models/userModel');
 const mongoose = require('mongoose');
 
-// GET /api/courses
-const list = async (req, res) => {
-  const courses = await Course.find({}).sort({createdAt: -1});
-  const camelCased = courses.map((course) => toCamelCase(course.toObject()));
-
-  res.status(200).json(camelCased);
-}
-
-// GET /api/courses/:id
-const get = async (req, res) => {
-  const {id}  = req.params;
-
-  if (!validId(id)) {
-    res.status(400).json({error: `${id} is not a valid course id.`});
-    return;
-  }
-
-  const course = await Course.findById(id);
-
-  if (!course) {
-    res.status(404).json({error: `Course with id ${id} was not found.`});
-    return;
-  }
-
-  res.status(200).json(toCamelCase(course.toObject()));
-}
-
-// DELETE /api/courses/:id
-const remove = async (req, res) => {
-  const {id} = req.params;
-
-  if (!validId(id)) {
-    res.status(400).json({error: `${id} is not a valid course id.`});
-    return;
-  }
-
-  const course = await Course.findOneAndDelete({_id: id})
-
-  if(!course) {
-    res.status(404).json({error: `Course with id ${id} was not found.`});
-    return;
-  }
-
-  res.status(200).json(toCamelCase(course.toObject()));
-}
-
-// POST /api/courses
-const create = async (req, res) => {
-  let {title,
-    providerId,
-    description,
-    shortDescription,
-    level,
-    price,
-    imageUrl,
-    maxStudents,
-    startDate,
-    endDate,
-    startTime,
-    endTime} = req.body;
+// POST /api/users
+const register = async (req, res) => {
+  let { name, email, phone, role=0 } = req.body;
 
   // Validate presence of data
-  // if (!title || !description || !shortDescription || !level) {
-  //   res.status(400).json({error: "Course must have title, description, shortDescription and level properties."});
-  //   return;
-  // }
-  if (!title) {
-    res.status(400).json({error: "Course must have a title."});
+  if (!name) {
+    res.status(400).json({error: "User must have a name."});
     return;
   }
 
   // Validate data types
   try {
-    if (price) { price = parseFloat(price); }
+    if (role) { role = parseInt(role); }
   } catch(err) {
-    res.status(400).json({error: `Failed to convert price/maxStudents/startTime/endTime into a number. Error: ${err}`});
-    return;
-  }
-
-  try {
-    if (startDate && typeof startDate === "string") { startDate = new Date(JSON.parse(startDate)); }
-    if (endDate && typeof endDate === "string") { endDate = new Date(JSON.parse(endDate)); }
-  } catch(err) {
-    res.status(400).json({error: `Failed to convert startDate/endDate to date. Error: ${err}`});
-    return;
-  }
-
-  if (providerId && !validId(providerId)) {
-    res.status(400).json({error: `${providerId} is not valid value for providerId.`});
+    res.status(400).json({error: `Failed to convert role into an integer. Error: ${err}`});
     return;
   }
 
   // Save to the database
   try {
     const payload = toUnderscoreCase({
-      title,
-      providerId,
-      description,
-      shortDescription,
-      level,
-      price,
-      imageUrl,
-      maxStudents,
-      startDate,
-      endDate,
-      startTime,
-      endTime
+      name,
+      email,
+      phone,
+      role
     });
 
-    payload.enrolled = [];
-
-    const course = await Course.create(payload);
-    res.status(200).json(toCamelCase(course.toObject()));
+    const user = await User.create(payload);
+    res.status(200).json(toCamelCase(user.toObject()));
   } catch (err) {
-    res.status(500).json({error: `Failed to save new course. Error: ${err}`});
+    res.status(500).json({error: `Failed register user. Error: ${err}`});
   }
 }
 
@@ -191,6 +107,25 @@ const update = async (req, res) => {
   } catch (err) {
     res.status(500).json({error: `Failed to save updates to the course. Error: ${err}`});
   }
+}
+
+// DELETE /api/courses/:id
+const remove = async (req, res) => {
+  const {id} = req.params;
+
+  if (!validId(id)) {
+    res.status(400).json({error: `${id} is not a valid course id.`});
+    return;
+  }
+
+  const course = await Course.findOneAndDelete({_id: id})
+
+  if(!course) {
+    res.status(404).json({error: `Course with id ${id} was not found.`});
+    return;
+  }
+
+  res.status(200).json(toCamelCase(course.toObject()));
 }
 
 const validId = (id) => {
--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -4,5 +4,5 @@ const userSchema = new Schema({
   name: {type: String, required: true},
   email: {type: String},
   phone: {type: String},
-  role: {type: Number, required: true}
+  role: {type: Number, required: true} // 0-customer, 1-provider
 }, {timestamps: true});
