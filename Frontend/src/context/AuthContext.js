2327ff5583c21e219d3df9177b817fdfdd8d402c
Arman Yerkesh <a.yerkesh@gmail.com>
2023-09-28 16:58:07 +0300

    Fix bug with user role after page reload

--- Frontend/src/context/AuthContext.js
+++ Frontend/src/context/AuthContext.js
@@ -3,17 +3,17 @@ import { createContext, useReducer, useEffect } from 'react';
 
 // Define role constants
 export const ROLE = {
-  CUSTOMER: 'CUSTOMER', 
-  TEACHER: 'TEACHER' 
+  CUSTOMER: 'CUSTOMER',
+  TEACHER: 'TEACHER'
 };
 
 export const AuthContext = createContext();
-  
+
 
 export const authReducer = (state, action) => {
-  console.log('Dispatching action:', action); 
+  console.log('Dispatching action:', action);
   console.log('Current state:', state);
-  
+
   switch (action.type) {
     case 'LOGIN':
       const newStateLogin = { ...state, user: action.payload };
@@ -21,7 +21,7 @@ export const authReducer = (state, action) => {
       return { ...state, user: action.payload };
     case 'LOGOUT':
       const newStateLogout = { ...state, user: null, role: null };
-      console.log('New state after LOGOUT:', newStateLogout); 
+      console.log('New state after LOGOUT:', newStateLogout);
       return { ...state, user: null, role: null };
     case 'SET_ROLE':
       const newStateRole = { ...state, role: action.payload };
@@ -43,14 +43,7 @@ export const AuthContextProvider = ({ children }) => {
 
     if (user) {
       dispatch({ type: 'LOGIN', payload: user });
-      
-      if (user.role === 0) {
-        dispatch({ type: 'SET_ROLE', payload: ROLE.CUSTOMER });
-      } else if (user.role === 1) {  // Assuming 1 is for TEACHER
-        dispatch({ type: 'SET_ROLE', payload: ROLE.TEACHER });
-      } else {
-        console.log('Invalid role:', user.role);
-      }
+      dispatch({ type: 'SET_ROLE', payload: user.role });
     }
   }, []);
   const fetchWithToken = async (url, options = {}) => {
@@ -59,7 +52,7 @@ export const AuthContextProvider = ({ children }) => {
       Authorization: `Bearer ${token}`,
       ...options.headers,
     };
-    
+
     const response = await fetch(url, { ...options, headers });
     return response;
   };
