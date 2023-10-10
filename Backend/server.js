18ab3b5e033426d08ac29c86f017ebb18fae9bbf
Arman Yerkesh <a.yerkesh@gmail.com>
2023-10-10 15:14:05 +0300

    Add documentation for courses resource

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const coursesRouter = require('./routes/coursesRouter');
 const usersRouter = require('./routes/usersRouter');
 const cors = require('cors');
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./config/swagger");
 
 // express app
 const app = express();
@@ -21,5 +23,7 @@ app.get('/', (req, res) => res.send('API Running!'));
 // routes
 app.use('/api', coursesRouter);
 app.use('/api/users', usersRouter);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
